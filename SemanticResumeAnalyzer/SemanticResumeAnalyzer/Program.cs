8f067b3f3764813fbbbc1cd08827509c19d52606
sonixJams <sonixjams@gmail.com>
2018-04-22 18:40:49 +0300

    Code addition: add text extractor and references

---
+++ SemanticResumeAnalyzer/SemanticResumeAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticResumeAnalyzer
+{
+    class Program
+    {
+        private const string pathToResume = "D:\\abc\\Sorin\\cv engleza.doc";
+        static void Main(string[] args)
+        {
+            TextExtractor textExtractor = new TextExtractor();
+            string text = textExtractor.GetText(pathToResume);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ SemanticResumeAnalyzer/SemanticResumeAnalyzer/TextExtractor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using Xceed.Words.NET;
+using Spire.Pdf;
+using WordInterop = Microsoft.Office.Interop.Word;
+using System.Text;
+
+namespace SemanticResumeAnalyzer
+{
+    public enum FileType
+    {
+        None = 0x00,
+        Pdf = 0x01,
+        Doc = 0x02,
+        DocX = 0x04
+    }
+
+    public class TextExtractor
+    {
+        public string FilePath { get; set; }
+        public string Text { get; set; }
+
+        public TextExtractor()
+        {
+            Text = string.Empty;
+            FilePath = string.Empty;
+        }
+
+        public string GetText(string filePath)
+        {
+            string text = null;
+            string fileExtension = null;
+            FileType fileType;
+  
+            fileExtension = Path.GetExtension(filePath);
+            if (string.IsNullOrEmpty(fileExtension))
+            {
+                throw new ArgumentException("The resume file has a invalid extension!");
+            }
+            
+            switch (fileExtension)
+            {
+                case ".pdf":
+                    fileType = FileType.Pdf;
+                    text = GetTextFromPdfFile(filePath);
+                    break;
+                case ".doc":
+                    fileType = FileType.Doc;
+                    text = GetTextFromDocFile(filePath);
+                    break;
+                case ".docx":
+                    fileType = FileType.DocX;
+                    text = GetTextFromDocXFile(filePath);
+                    break;
+            }
+
+            return text;
+        }
+
+        private string GetTextFromPdfFile(string filePath)
+        {
+            PdfDocument pdfDoc = new PdfDocument();
+            pdfDoc.LoadFromFile(filePath);
+
+            string text = string.Empty;
+            StringBuilder buff = new StringBuilder();
+
+            foreach (PdfPageBase page in pdfDoc.Pages)
+            {
+                buff.Append(page.ExtractText());
+            }
+
+            text = buff.ToString();
+            pdfDoc.Close();
+
+            return text;
+        }
+        private string GetTextFromDocFile(string filePath)
+        {
+            WordInterop.Application app = new WordInterop.Application();
+            WordInterop.Document doc = app.Documents.Open(filePath);
+
+            string text = string.Empty;
+
+            foreach (WordInterop.Paragraph p in doc.Paragraphs)
+            {
+                text += p.Range.Text;
+            }
+
+            doc.Close();
+            app.Quit();
+
+            return text;
+        }
+
+        private string GetTextFromDocXFile(string filePath)
+        {
+            string text = string.Empty;
+
+            using (DocX docx = DocX.Load(filePath))
+            {
+                foreach (Paragraph p in docx.Paragraphs)
+                {
+                    text += (string.IsNullOrEmpty(text) ? string.Empty : Environment.NewLine) + p.Text;
+                }
+            }
+
+            return text;
+        }
+
+    }
+}
